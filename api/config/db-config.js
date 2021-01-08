0b05968bf3b9eebb562e205901017812ccab28bf
Yasser Nabil <ynabil14@gmail.com>
2021-01-08 19:14:53 +0100

    add socket path

--- api/config/db-config.js
+++ api/config/db-config.js
@@ -7,6 +7,7 @@ const connection = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    socketPath: process.env.SOCKET_PATH,
     multipleStatements: true
 });
 connection.getConnection(function(err) {
