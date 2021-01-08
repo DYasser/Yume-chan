3b52a64d03e2c53c18f1f27a2a97f523d1a4ea19
Yasser Nabil <ynabil14@gmail.com>
2021-01-08 22:38:16 +0100

    updt hello command

--- main.js
+++ main.js
@@ -41,8 +41,6 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift();//.toLowerCase();
 
-    console.log(command);
-
     if(command === 'ping'){
         client.commands.get('ping').execute(message, args);
     } else if(command === 'hello'){
