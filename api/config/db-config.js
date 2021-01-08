a5d570e44206f855566e6384393b4a002317ca9c
Yasser Nabil <ynabil14@gmail.com>
2021-01-08 20:03:30 +0100

    Update db-config.js

--- api/config/db-config.js
+++ api/config/db-config.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql');
 
 // Configuration to connect to the database ; for more info see the environnement variables
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     socketPath: process.env.SOCKET_PATH
 });
-connection.connect(function(err) {
+connection.getConnection(function(err) {
     if (err) {
         console.error('error connecting: ' + err.message);
         return;
