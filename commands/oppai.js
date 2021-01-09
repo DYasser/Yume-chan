c0bf2527e348c340fe69aa7b427dd4d24387f824
Yasser Nabil <ynabil14@gmail.com>
2021-01-09 22:11:50 +0100

    Update oppai.js

--- commands/oppai.js
+++ commands/oppai.js
@@ -6,30 +6,38 @@ module.exports = {
     description: 'NSFW! some oppais ... I guess I am not enough',
     type: 'stuff',
     execute(message, args){
-        const embed = new Discord.MessageEmbed();
-	    got('https://www.reddit.com/r/AnimeTitties/random/.json')
-		.then(response => {
-			const [list] = JSON.parse(response.body);
-			const [post] = list.data.children;
-			
+		const embed = new Discord.MessageEmbed();
 
-			const permalink = post.data.permalink;
-			const memeUrl = `https://reddit.com${permalink}`;
-			const memeImage = post.data.url;
-			console.log(memeImage);
-			const memeTitle = post.data.title;
-			const memeUpvotes = post.data.ups;
-			const memeNumComments = post.data.num_comments;
 
-			embed.setTitle(`${memeTitle}`);
-			embed.setURL(`${memeUrl}`);
-			embed.setColor('RANDOM');
-			embed.setImage(memeImage);
-			embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
 
-			message.channel.send(embed);
-		})
-		.catch(console.error);
+		function oppai(){
+			got('https://www.reddit.com/r/AnimeTitties/random/.json')
+			.then(response => {
+				const [list] = JSON.parse(response.body);
+				const [post] = list.data.children;
+				
+	
+				const permalink = post.data.permalink;
+				const memeUrl = `https://reddit.com${permalink}`;
+				const memeImage = post.data.url;
+				if(memeImage[8]!== "i"){
+					console.log("nope");
+				}
+				const memeTitle = post.data.title;
+				const memeUpvotes = post.data.ups;
+				const memeNumComments = post.data.num_comments;
+	
+				embed.setTitle(`${memeTitle}`);
+				embed.setURL(`${memeUrl}`);
+				embed.setColor('RANDOM');
+				embed.setImage(memeImage);
+				embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+	
+				message.channel.send(embed);
+			})
+			.catch(console.error);
+		}
+	    
     }
 }
 
