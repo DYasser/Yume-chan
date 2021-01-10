5dd697a265c00a41d78a26f2a53400f1d0e2aa6a
Yasser Nabil <ynabil14@gmail.com>
2021-01-10 12:00:36 +0100

    Update oppai.js

--- commands/oppai.js
+++ commands/oppai.js
@@ -22,7 +22,7 @@ module.exports = {
 				console.log(permalink);
 				console.log(memeImage);
 				console.log("------------------");
-				if(memeImage[8]!== "i"){
+				if(memeImage[8]!== "i" && memeImage[9]!== "." && memeImage[10]!== "i"){
 					oppai();
 					memeImage = "";
 					console.log("testing");
