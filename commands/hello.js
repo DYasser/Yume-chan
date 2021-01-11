328a4ba37eabaa184a4815755d81fae5999162e6
Yasser Nabil <ynabil14@gmail.com>
2021-01-11 15:37:24 +0100

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -3,7 +3,29 @@ module.exports = {
     description: 'I come up with a greeting for my master',
     type: 'stuff',
     execute(message, args){
-        message.react('🥰');
-        message.channel.send("Hello master :heart:");
+        message.react('💖');
+        
+        let randomMsg = Math.floor(Math.random*10)%6;
+
+        switch(randomMsg){
+            case 0:
+                message.channel.send("Hello master :heart:");
+                break;
+            case 1:
+                message.channel.send("Ohayoo~~ I hope you're doing great :heart:");
+                break;
+            case 2:
+                message.channel.send("Wsap aniki :heart:");
+                break;
+            case 3:
+                message.channel.send("Senpai~~ Ohayo :heart:");
+                break;
+            case 4:
+                message.channel.send("Onii~chan Ohayo :heart:");
+                break;
+            case 5:
+                message.channel.send("Hii~ hope u're feeling good :heart:");
+                break;
+        }
     }
 }
