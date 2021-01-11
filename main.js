204e3ae4d71421912b3f0efb6cfed093b482abe5
Yasser Nabil <ynabil14@gmail.com>
2021-01-11 16:07:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift();//.toLowerCase();
 
-    if(asleep && command !== "goodMorning") {
+    if(asleep && command !== "goodmorning") {
         message.channel.send("Zzzz");
         return;
     }
